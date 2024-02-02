e3c7fadaaf44d649a739c74e69f0e06d65829c0a
atifachitthi <atifachitthi2512@gmail.com>
2024-02-02 15:08:24 +0800

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersPageComponent } from './MyComponents/orders-page/orders-page.component';
+import { TopHeaderComponent } from './MyComponents/top-header/top-header.component';
+import { SideHeaderComponent } from './MyComponents/side-header/side-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersPageComponent,
+    TopHeaderComponent,
+    SideHeaderComponent
   ],
   imports: [
     BrowserModule,
